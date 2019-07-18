c1fc44de046cdbf6f18f4ccc0c93d1f5a22a86ec
ududit007 <ududit007@gmail.com>
2019-07-18 13:26:10 +0530

    added email detail in .env

--- scrapper/settings/local.py
+++ scrapper/settings/local.py
@@ -2,10 +2,3 @@ from .base import *
 
 
 DEBUG = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'uditmindfire@gmail.com'
-EMAIL_HOST_PASSWORD = 'jwknisvzmophqkzh'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
